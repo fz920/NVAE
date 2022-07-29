7db1075663deb901807726dbe49fb780a841f132
fz920 <97394947+fz920@users.noreply.github.com>
2022-07-29 13:55:31 +0100

    update

--- cifar10.sh
+++ cifar10.sh
@@ -7,8 +7,8 @@ export EXPR_ID=UNIQUE_EXPR_ID
 export DATA_DIR=PATH_TO_DATA_DIR
 export CHECKPOINT_DIR=PATH_TO_CHECKPOINT_DIR
 export CODE_DIR=PATH_TO_CODE_DIR
-CUDA_VISIBLE_DEVICES=1 python train.py --data $DATA_DIR/cifar10 --root $CHECKPOINT_DIR --save $EXPR_ID --dataset cifar10 \
-        --num_channels_enc 128 --num_channels_dec 128 --epochs 400 --num_postprocess_cells 2 --num_preprocess_cells 2 \
+CUDA_VISIBLE_DEVICES=1 CUDA_LAUNCH_BLOCKING=1 python train.py --data $DATA_DIR/cifar10 --root $CHECKPOINT_DIR --save $EXPR_ID --dataset cifar10 \
+        --num_channels_enc 128 --num_channels_dec 128 --epochs 1 --num_postprocess_cells 2 --num_preprocess_cells 2 \
         --num_latent_scales 1 --num_latent_per_group 20 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 \
         --num_preprocess_blocks 1 --num_postprocess_blocks 1 --num_groups_per_scale 30 --batch_size 32 \
-        --weight_decay_norm 1e-2 --num_nf 1 --num_process_per_node 8 --use_se --res_dist --fast_adamax 
+        --weight_decay_norm 1e-2 --num_nf 1 --num_process_per_node 1 --use_se --res_dist --fast_adamax 
