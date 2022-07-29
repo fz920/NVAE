3755c79399d1bc0e2f2f24fa2685ee4c37c6a3b0
fz920 <97394947+fz920@users.noreply.github.com>
2022-07-29 14:51:15 +0100

    p

--- cifar10.sh
+++ cifar10.sh
@@ -11,4 +11,4 @@ CUDA_VISIBLE_DEVICES=3 python -m torch.distributed.launch --master_port 1234 tra
         --num_channels_enc 16 --num_channels_dec 16 --epochs 1 --num_postprocess_cells 2 --num_preprocess_cells 2 \
         --num_latent_scales 1 --num_latent_per_group 20 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 \
         --num_preprocess_blocks 1 --num_postprocess_blocks 1 --num_groups_per_scale 30 --batch_size 32 \
-        --weight_decay_norm 1e-2 --num_nf 0 --num_process_per_node 1 --use_se --res_dist --fast_adamax 
+        --weight_decay_norm 1e-2 --num_nf 0 --num_process_per_node 2 --use_se --res_dist --fast_adamax 
