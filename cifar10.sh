614eda980de4aec12ec051cd5f1f79449dc94268
fz920 <97394947+fz920@users.noreply.github.com>
2022-07-31 17:39:43 +0100

    p

--- cifar10.sh
+++ cifar10.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export EXPR_ID=1
+export EXPR_ID=2
 export DATA_DIR=/data2/users/fz920/data
 export CHECKPOINT_DIR=/data2/users/fz920/NVAE/checkpoint
 export CODE_DIR=/data2/users/fz920/NVAE
-CUDA_VISIBLE_DEVICES=1 python -m torch.distributed.launch --master_port 1234 train.py --data $DATA_DIR/cifar10 --root $CHECKPOINT_DIR --save $EXPR_ID --dataset cifar10 \
+CUDA_VISIBLE_DEVICES=1 python -m torch.distributed.launch --master_port 1235 train.py --data $DATA_DIR/cifar10 --root $CHECKPOINT_DIR --save $EXPR_ID --dataset cifar10 \
         --num_channels_enc 16 --num_channels_dec 16 --epochs 1 --num_postprocess_cells 2 --num_preprocess_cells 2 \
         --num_latent_scales 1 --num_latent_per_group 20 --num_cell_per_cond_enc 1 --num_cell_per_cond_dec 1 \
         --num_preprocess_blocks 1 --num_postprocess_blocks 1 --num_groups_per_scale 10 --batch_size 32 \
