abf50134f5a4cea2d6da5b81e307a17f2957f026
fz920 <97394947+fz920@users.noreply.github.com>
2022-07-31 23:11:19 +0100

    update

---
+++ check.sh
@@ -0,0 +1,6 @@
+export EXPR_ID=2
+export DATA_DIR=/data/users/fz920/data
+export CHECKPOINT_DIR=/data/users/fz920/NVAE/checkpoint
+export CODE_DIR=/data/users/fz920/NVAE
+
+python evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --eval_mode=sample --temp=0.6 --readjust_bn
--- cifar10.sh
+++ cifar10.sh
@@ -5,7 +5,7 @@ export CHECKPOINT_DIR=/data/users/fz920/NVAE/checkpoint
 export CODE_DIR=/data/users/fz920/NVAE
 cd $CODE_DIR
 CUDA_VISIBLE_DEVICES=1 python train.py --data $DATA_DIR/cifar10 --root $CHECKPOINT_DIR --save $EXPR_ID --dataset cifar10 \
-        --num_channels_enc 16 --num_channels_dec 16 --epochs 1 --num_postprocess_cells 2 --num_preprocess_cells 2 \
+        --num_channels_enc 32 --num_channels_dec 32 --epochs 10 --num_postprocess_cells 2 --num_preprocess_cells 2 \
         --num_latent_scales 1 --num_latent_per_group 20 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 \
-        --num_preprocess_blocks 1 --num_postprocess_blocks 1 --num_groups_per_scale 20 --batch_size 32 \
+        --num_preprocess_blocks 1 --num_postprocess_blocks 1 --num_groups_per_scale 10 --batch_size 32 \
         --weight_decay_norm 1e-2 --num_nf 0 --num_process_per_node 1 --use_se --res_dist --fast_adamax 
