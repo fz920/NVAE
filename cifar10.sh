7e10319c2c50c82f588d52709ad8a91f8d325e5b
fz920 <97394947+fz920@users.noreply.github.com>
2022-07-31 14:37:39 +0100

    p

--- cifar10.sh
+++ cifar10.sh
@@ -7,4 +7,4 @@ CUDA_VISIBLE_DEVICES=1 python train.py --data $DATA_DIR/cifar10 --root $CHECKPOI
         --num_channels_enc 16 --num_channels_dec 16 --epochs 1 --num_postprocess_cells 2 --num_preprocess_cells 2 \
         --num_latent_scales 1 --num_latent_per_group 20 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 \
         --num_preprocess_blocks 1 --num_postprocess_blocks 1 --num_groups_per_scale 30 --batch_size 32 \
-        --weight_decay_norm 1e-2 --num_nf 0 --num_process_per_node 4 --use_se --res_dist --fast_adamax 
+        --weight_decay_norm 1e-2 --num_nf 0 --num_process_per_node 1 --use_se --res_dist --fast_adamax 
