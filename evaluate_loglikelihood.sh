ce09bcb673a3e13b96b0be435d10ce5b80b50833
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-01 12:10:22 +0100

    update

--- evaluate_loglikelihood.sh
+++ evaluate_loglikelihood.sh
@@ -4,4 +4,4 @@ export CHECKPOINT_DIR=/data/users/fz920/NVAE/checkpoint
 export CODE_DIR=/data/users/fz920/NVAE
 
 cd $CODE_DIR
-python evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --data $DATA_DIR/mnist --eval_mode=evaluate --num_iw_samples=1000
+python evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --data $DATA_DIR/cifar10 --eval_mode=evaluate --num_iw_samples=1000
