260171426f332eed85b3f7df65a9fb1a2775a926
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-01 16:48:48 +0100

    update

--- evaluate_loglikelihood.sh
+++ evaluate_loglikelihood.sh
@@ -1,7 +1,7 @@
-export EXPR_ID=3
+export EXPR_ID=4
 export DATA_DIR=/data/users/fz920/data
 export CHECKPOINT_DIR=/data/users/fz920/NVAE/checkpoint
 export CODE_DIR=/data/users/fz920/NVAE
 
 cd $CODE_DIR
-CUDA_VISIBLE_DEVICES=1 python evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --data $DATA_DIR/cifar10 --eval_mode=evaluate --num_iw_samples=1000
+python evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --data $DATA_DIR/cifar10 --eval_mode=evaluate --num_iw_samples=1000
