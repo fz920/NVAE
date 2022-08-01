814baad26f245c8be619d7184155045f8bb446fd
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-01 22:36:47 +0100

    update e

--- evaluate_fid.sh
+++ evaluate_fid.sh
@@ -6,4 +6,4 @@ export CODE_DIR=/data/users/fz920/NVAE
 cd $CODE_DIR
 
 python scripts/precompute_fid_statistics.py --data $DATA_DIR/cifar10 --dataset cifar10 --fid_dir /tmp/fid-stats/
-python evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --data $DATA_DIR/cifar10 --eval_mode=evaluate_fid  --fid_dir /tmp/fid-stats/ --temp=0.6 --readjust_bn
+python torch.distributed.launch --nproc_per_node=1 --master_port 1102 evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --data $DATA_DIR/cifar10 --eval_mode=evaluate_fid  --fid_dir /tmp/fid-stats/ --temp=0.6 --readjust_bn
