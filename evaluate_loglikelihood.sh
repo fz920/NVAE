76d287827bb8f72440f515c6e0f021e7a7368734
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-02 09:48:29 +0100

    f

--- evaluate_loglikelihood.sh
+++ evaluate_loglikelihood.sh
@@ -4,4 +4,4 @@ export CHECKPOINT_DIR=/data/users/fz920/NVAE/checkpoint
 export CODE_DIR=/data/users/fz920/NVAE
 
 cd $CODE_DIR
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port 1100 evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --data $DATA_DIR/cifar10 --eval_mode=evaluate --num_iw_samples=1000
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port 1238 evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --data $DATA_DIR/cifar10 --eval_mode=evaluate --num_iw_samples=1000
