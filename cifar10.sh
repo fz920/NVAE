bb70160a728e9d8d3ad32a90dd4dfb3dbc7c9dfd
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-03 21:05:00 +0100

    f

--- cifar10.sh
+++ cifar10.sh
@@ -5,7 +5,7 @@ export CHECKPOINT_DIR=/data/users/fz920/NVAE/checkpoint
 export CODE_DIR=/data/users/fz920/NVAE
 cd $CODE_DIR
 CUDA_VISIBLE_DEVICES=1 python train.py --data $DATA_DIR/cifar10 --root $CHECKPOINT_DIR --save $EXPR_ID --dataset cifar10 \
-        --num_channels_enc 64 --num_channels_dec 64 --epochs 40 --num_postprocess_cells 2 --num_preprocess_cells 2 \
+        --num_channels_enc 64 --num_channels_dec 64 --epochs 100 --num_postprocess_cells 2 --num_preprocess_cells 2 \
         --num_latent_scales 1 --num_latent_per_group 20 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 \
         --num_preprocess_blocks 1 --num_postprocess_blocks 1 --num_groups_per_scale 30 --batch_size 32 \
         --weight_decay_norm 1e-2 --num_nf 1 --num_process_per_node 1 --use_se --res_dist --fast_adamax --cont_training
