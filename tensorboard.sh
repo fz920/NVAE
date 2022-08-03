5ca76e153fb1c16b5d21876609dc2463fa8e5369
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-03 21:41:23 +0100

    d

---
+++ tensorboard.sh
@@ -0,0 +1,6 @@
+export EXPR_ID=5
+export DATA_DIR=/data/users/fz920/data
+export CHECKPOINT_DIR=/data/users/fz920/NVAE/checkpoint
+export CODE_DIR=/data/users/fz920/NVAE
+
+tensorboard --logdir $CHECKPOINT_DIR/eval-$EXPR_ID/
