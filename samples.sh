8df48b61611a6a01fccf228bed5217626ab11bf6
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-11 22:50:17 +0100

    f

--- samples.sh
+++ samples.sh
@@ -3,4 +3,4 @@ export DATA_DIR=/data/users/fz920/data
 export CHECKPOINT_DIR=/data/users/fz920/NVAE/checkpoint
 export CODE_DIR=/data/users/fz920/NVAE
 
-CUDA_VISIBLE_DEVICES=0 python3 evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --eval_mode=sample --temp=0.6 --readjust_bn
+CUDA_VISIBLE_DEVICES=1 python3 evaluate.py --checkpoint $CHECKPOINT_DIR/eval-$EXPR_ID/checkpoint.pt --eval_mode=sample --temp=0.6 --readjust_bn
