2f17f497f959b11485aabddd75fd4e9b2145a3c3
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-17 09:49:15 +0100

    f

--- celea.sh
+++
@@ -1,7 +0,0 @@
-export DATA_DIR=/data/users/fz920/data
-export CODE_DIR=/data/users/fz920/NVAE
-
-cd $CODE_DIR/scripts
-python create_celeba64_lmdb.py --split train --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
-python create_celeba64_lmdb.py --split valid --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
-python create_celeba64_lmdb.py --split test  --img_path $DATA_DIR/celeba_org --lmdb_path $DATA_DIR/celeba64_lmdb
