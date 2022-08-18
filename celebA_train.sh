d7bc2e6c70709bdf88b9ff37d360fd0d14d4846b
fz920 <97394947+fz920@users.noreply.github.com>
2022-08-18 14:31:21 +0100

    f

--- celebA_train.sh
+++ celebA_train.sh
@@ -5,8 +5,8 @@ export CODE_DIR=/data/users/fz920/NVAE
 
 cd $CODE_DIR
 
-CUDA_VISIBLE_DEVICES=0,1 python3 train.py --data $DATA_DIR/celeba64_lmdb --root $CHECKPOINT_DIR --save $EXPR_ID --dataset celeba_64 \
+CUDA_VISIBLE_DEVICES=1 python3 train.py --data $DATA_DIR/celeba64_lmdb --root $CHECKPOINT_DIR --save $EXPR_ID --dataset celeba_64 \
         --num_channels_enc 32 --num_channels_dec 32 --epochs 20 --num_postprocess_cells 2 --num_preprocess_cells 2 \
         --num_latent_scales 3 --num_latent_per_group 20 --num_cell_per_cond_enc 2 --num_cell_per_cond_dec 2 \
         --num_preprocess_blocks 1 --num_postprocess_blocks 1 --weight_decay_norm 1e-1 --num_groups_per_scale 20 \
-        --batch_size 16 --num_nf 1 --ada_groups --num_process_per_node 2 --use_se --res_dist --fast_adamax --cont_training
+        --batch_size 16 --num_nf 1 --ada_groups --num_process_per_node 1 --use_se --res_dist --fast_adamax --cont_training
